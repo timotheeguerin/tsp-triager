3a23a40b432ba26c9e1cfa9305e8fd457c17a492
Timothee Guerin <tiguerin@microsoft.com>
2026-02-11 15:38:52 -0500

    Deploy to github actions

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/tsp-triager/",
   plugins: [react()],
   root: ".",
   build: {
